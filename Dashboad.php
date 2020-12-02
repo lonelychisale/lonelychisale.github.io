6e7068b70c7e32ddeb84011535955f9091c5bde3
Madalitso Nyemba <mnyemba@bintelanalytics.mw>
2020-12-02 21:59:51 +0200

    DOne

---
+++ Dashboad.php
@@ -0,0 +1,86 @@
+
+<html>
+    <head>
+        
+        <style type="text/css">
+
+             
+
+        input{padding: 20px;
+        color: white; 
+        border-bottom: 3px  solid violet;
+         background-color: transparent;
+         border: none;
+          color: white;
+          width: 70%;
+        
+    }
+    button{
+        background-color: violet;
+         border-radius: 40px;
+         width:70% ;
+       margin-top: 15%;
+    }
+    label{  font-family:Arial ;
+    font-size: 30px;
+    color: white;}
+
+    form{background-color: burlywood;
+        background-image: url(jj1.jpg);
+        opacity: 2px;
+         width: 450px;
+         height: 450px;
+         text-align: center;
+         border: 3px solid white;
+       
+    }
+  
+    body{
+        color:black;;
+    }
+
+    p{font-size: 60px;
+    font-family: Baskerville Old Face;
+    color:black;}
+    #ss{font-size: 100px;
+    color: chocolate;
+    font-family: Edwardian Script ITC;}
+    #ll{background-color: cadetblue;
+  
+    }
+    div{padding-top: 5%;padding-left: 30%}
+    button{background-color: violet;
+    border-radius: 40px;
+    color: white;
+    font-size: 30px;
+    
+    }
+
+#m{background-color: burlywood;}
+   
+    
+    
+         </style>
+    </head>
+        <body >
+            <marquee>
+            <P id="ss">You are most welcome to my website</P></marquee>
+            <div id="ll">
+<p> Have you ever been  interested in learning programming??</br>Dont miss this chance
+<br><strong style="color: violet;">Register with us below</strong></p></div>
+<div  id="m">
+<form  action="server2.php" method="POST"><br><br>
+    <label for="name" >Firstname</label><br>
+    <input type="text"   name="name" required><br><br>
+    <label for="surname" >surname</label><br>
+    <input type="text"  name="surname" required><br><br>
+    <button    name="register">register</button> 
+    
+</form>
+</div>
+
+
+            
+        </body>
+    </head>
+</html>
